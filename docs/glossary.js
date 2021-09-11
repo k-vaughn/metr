62e1b4a5520964ea7b87e1af554500098badfdd0
Kenneth Vaughn <kvaughn@trevilon.com>
2021-09-10 19:34:33 -0500

    Update with details, images, tooltip json script

---
+++ docs/glossary.js
@@ -0,0 +1,22 @@
+fetch('glossary.json')
+	.then(response => response.json())
+	.then(result => populateData(result))
+
+function populateData(data) {
+	for (var i = 0; i < data.length; i++) {
+		var entry = data[i];
+		var els = document.getElementsByClassName("tooltip");
+		for (var j = 0; j < els.length; j++) {
+			var field = els[j];
+			var t = field.innerHTML;
+			var term = field.getAttribute("term")
+			if (!term) {
+				term = t;
+			}
+			if (term == entry.id) {
+				field.innerHTML = '<a href="glossary.html#' + entry.id + '" title="' + entry.defn + '">' + t + '</a>';
+			}
+		}
+	}
+}
+
